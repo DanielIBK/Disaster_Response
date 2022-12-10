2762ed6527b8a703180d901e297eb2b9cbaa8c96
danIBK <heuserdaniel.dh@gmail.com>
2022-12-10 18:06:59 +0100

    updates

--- data/process_data.py
+++ data/process_data.py
@@ -6,7 +6,8 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    """
+    """Load the necessary data.
+
     Loads the message file and the categorrie file 
     and merge them into one Dataframe based on a inner join on the column name 'id'
 
@@ -24,15 +25,65 @@ def load_data(messages_filepath, categories_filepath):
     return df
 
 def clean_data(df):
+    """Clean the loaded data.
+    
+    Function:
+      Cleans the Dataframe df and combines them into one large df
+
+    Args:
+        df (dataframe): _description_
+
+    Return:
+      df (DataFrame): A cleaned dataframe with merged data from messages and categories
+    """
     # Split the multi column 'categories' into separate category columns based on seperater ';'.
     categories = df['categories'].str.split(';', expand=True)
 
+    # select the first row of the categories dataframe
+    row = categories.head(1)
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.applymap(lambda x: x[:-2]).iloc[0,:]
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+    
+    
+    # drop the original categories column from `df`
+    df.drop(columns=['categories'], inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1, join='inner')
+    
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    return
 
 def save_data(df, database_filename):
-    pass  
+    """Safe the data as SQL Lite.
+
+    Args:
+        df (_type_): _description_
+        database_filename (_type_): _description_
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    try: 
+        df.to_sql('tbl_disastermessages', engine, index=False)  
+    except:
+        print("Exception: The table seems to exists already")
 
 
 def main():
+    """Start the necessary function for the Main module."""
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
